4408a93246e38e667c4e3ae189b2810e642a5c33
40012435 <a.myint@napier.ac.uk>
2022-03-31 16:14:38 +0630

    merging with master added

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,75 +7,82 @@ import java.util.ArrayList;
 /* it is App who connect to MySql  and extract one application */
 public class App
 {  Connection con = null;
-    public void connect()
-    {
-        try
-        {
+    public void connect(String location, int delay) {
+        try {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        // Connection to the database
-
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
+        int retries = 10;
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
-                Thread.sleep(30000);
+                Thread.sleep(delay);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
-                System.out.println("Successfully connected to world sql");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
+                con = DriverManager.getConnection("jdbc:mysql://" + location
+                                + "/world?allowPublicKeyRetrieval=true&useSSL=false",
+                        "root", "example");
+                System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+            } catch (SQLException sqle) {
+                System.out.println("Failed to connect to database attempt " +                                  Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
-
-
     }
-    public ArrayList<CapitalCityReport>  getCitiesBtPop() throws SQLException {    String sql ="select c.Name,co.Name,c.Population from country as co, city as c where co.Code =c.CountryCode order by c.Population desc limit 5";
+
+
+    public ArrayList<CapitalCityReport>  getCitiesBtPop() throws SQLException {
+
+        String sql ="select c.Name,co.Name,c.Population from country as co, city as c where co.Code =c.CountryCode order by c.Population desc limit 5";
         PreparedStatement pstmt = con.prepareStatement(sql);
         ResultSet rset = pstmt.executeQuery();
-        ArrayList<CapitalCityReport>  capitals = new ArrayList<CapitalCityReport>();
+       ArrayList <CapitalCityReport>  capitals = new ArrayList<CapitalCityReport>();
         while(rset.next())
         {
-            capitals.add(new CapitalCityReport(rset.getString(1),rset.getString(2),rset.getString(3)));
+           capitals.add(new CapitalCityReport(rset.getString(1),rset.getString(2),rset.getString(3)));
         }
     return capitals;
     }
     public void displayCapitalCity(ArrayList<CapitalCityReport> capitals)
-    {
-       for (CapitalCityReport crp: capitals)
-       {
-           System.out.println(crp.getName()+"\t"+crp.getCountry()+"\t"+crp.getPopulation());
-       }
+    {   if (capitals ==null)
+        {
+        System.out.println(" capital is null");
+        return;
+         }
+         else if (capitals.size() == 0)
+         {   System.out.println(" There is no country data ");
+                return;
 
+            }
+         else {
+            for (CapitalCityReport crp : capitals)
+            {
+                if (crp == null) System.out.println(" capital is null ");
+                else {
+                System.out.println(crp.getName() + "\t" + crp.getCountry() + "\t" + crp.getPopulation());
+            }
+        }
+    }
     }
     public static void main(String[] args) throws SQLException {
-        App app= new App();
-        app.connect();
+        App app = new App();
+
+        if(args.length < 1){
+            app.connect("localhost:33060", 0);
+        }else{
+            app.connect("db:3306", 30000);
+        }
         ArrayList<Country> courntryList = app.getAllCountriesInfobyPopulation();
         app.display(courntryList);
-
-
+        ArrayList<CapitalCityReport> creport = app.getCitiesBtPop();
+        app.displayCapitalCity(creport);
         app.disConnect();
 
     }
@@ -105,13 +112,24 @@ public class App
             countries.add(c);
 
         }
+        countries.add(null);// for testing purposes
         return countries;
     }
-    public void display(ArrayList<Country> countries)
-    {   for(Country c: countries)
-      {
-            System.out.println(c.getName()+"\t"+c.getContinent()+"\t"+c.getCapital()+"\t"+c.getRegion()+"\t"+c.getPopulation());
-         }
+    public void display(ArrayList<Country> countries) {
+        if (countries == null)
+            System.out.println(" countries object is null");
+        else if (countries.size()==0)
+        {
+            System.out.println(" no country");
+        }
+        else {
+            for (Country c : countries) {
+                if (c == null) System.out.println(" country object is null");
+                else {
+                    System.out.println(c.getName() + "\t" + c.getContinent() + "\t" + c.getCapital() + "\t" + c.getRegion() + "\t" + c.getPopulation());
+                }
+            }
 
+        }
     }
 }
